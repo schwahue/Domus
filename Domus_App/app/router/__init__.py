bb8131a4cf8c4b16d9cba6e059526da85caa420c
Farah <191784K@mymail.nyp.edu.sg>
2021-12-27 10:01:13 +0800

    Layout for Listings page
    Simple and basic

--- Domus_App/app/router/__init__.py
+++ Domus_App/app/router/__init__.py
@@ -5,3 +5,7 @@ router = Blueprint('router', __name__, template_folder='templates')
 @router.route('/')
 def index():
     return render_template("index.html"), 200
+
+@router.route('/listings')
+def listings():
+    return render_template("listings.html"), 200
